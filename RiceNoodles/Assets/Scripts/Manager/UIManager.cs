42ebc77080b0c38bca3dcd9a56398908b8c74c38
jhoh0518 <jhoh0518@gmail.com>
2024-08-29 16:41:28 +0900

    Add infos

--- RiceNoodles/Assets/Scripts/Manager/UIManager.cs
+++ RiceNoodles/Assets/Scripts/Manager/UIManager.cs
@@ -7,22 +7,29 @@ using UnityEditor.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private Camera mainCamera;
+
     [SerializeField] private Canvas todayResultCanvas;
     [SerializeField] private Canvas MoneyCanvas;
     [SerializeField] private Canvas ButtonCanvas;
     [SerializeField] private Canvas EnchantCanvas;
     [SerializeField] private Canvas SettingsCanvas;
     [SerializeField] private Canvas TimeCanvas;
+    [SerializeField] private Canvas InfoCanvas;
 
     [SerializeField] private Sprite[] ClockSprites;
 
     private TextMeshProUGUI moneyText;
     private TextMeshProUGUI todayResultText;
+    private TextMeshProUGUI dayInfoText;
+    private TextMeshProUGUI targetText;
 
     private Button settingButton;
     private Button enchantButton;
 
     private Image clockImage;
+    private Image targetFrame;
+    private Image blackScreen;
 
     private static string previousState;
     private static string currentState;
@@ -36,6 +43,8 @@ public class UIManager : MonoBehaviour
 
         moneyText = MoneyCanvas.transform.Find("Money/Amount").GetComponent<TextMeshProUGUI>();
         todayResultText = todayResultCanvas.transform.Find("Text").GetComponent<TextMeshProUGUI>();
+        dayInfoText = InfoCanvas.transform.Find("Day").GetComponent<TextMeshProUGUI>();
+        targetText = InfoCanvas.transform.Find("Target/Target Text").GetComponent<TextMeshProUGUI>();
 
         settingButton = ButtonCanvas.transform.Find("Settings").GetComponent<Button>();
         enchantButton = ButtonCanvas.transform.Find("Enchant").GetComponent<Button>();
@@ -43,6 +52,8 @@ public class UIManager : MonoBehaviour
         enchantButton.onClick.AddListener(EnchantButtonClick);
 
         clockImage = TimeCanvas.transform.Find("Clock").GetComponent<Image>();
+        targetFrame = InfoCanvas.transform.Find("Target/Target Frame").GetComponent<Image>();
+        blackScreen = InfoCanvas.transform.Find("Black Screen").GetComponent<Image>();
 
         MoneyCanvas.gameObject.SetActive(false);
         todayResultCanvas.gameObject.SetActive(false);
@@ -54,6 +65,7 @@ public class UIManager : MonoBehaviour
 
     private void Start(){
         currentState = "InGame";
+        ShowInfoUI();
     }
 
 
@@ -88,7 +100,7 @@ public class UIManager : MonoBehaviour
                 EnchantCanvas.gameObject.SetActive(false);
                 SettingsCanvas.gameObject.SetActive(false);
                 TimeCanvas.gameObject.SetActive(true);
-
+                InfoCanvas.gameObject.SetActive(true);
                 //Debug.Log(TimeManager.CurrentHour);
                 clockImage.sprite = ClockSprites[TimeManager.CurrentHour-10];
                 break;
@@ -99,6 +111,7 @@ public class UIManager : MonoBehaviour
                 EnchantCanvas.gameObject.SetActive(false);
                 SettingsCanvas.gameObject.SetActive(false);
                 TimeCanvas.gameObject.SetActive(false);
+                InfoCanvas.gameObject.SetActive(false);
                 break;
             case "Settings":
                 MoneyCanvas.gameObject.SetActive(false);
@@ -107,6 +120,7 @@ public class UIManager : MonoBehaviour
                 EnchantCanvas.gameObject.SetActive(false);
                 SettingsCanvas.gameObject.SetActive(true);
                 TimeCanvas.gameObject.SetActive(false);
+                InfoCanvas.gameObject.SetActive(false);
                 break;
             case "Enchant":
                 MoneyCanvas.gameObject.SetActive(false);
@@ -115,6 +129,7 @@ public class UIManager : MonoBehaviour
                 EnchantCanvas.gameObject.SetActive(true);
                 SettingsCanvas.gameObject.SetActive(false);
                 TimeCanvas.gameObject.SetActive(false);
+                InfoCanvas.gameObject.SetActive(false);
                 break;
         }
     }
@@ -153,6 +168,7 @@ public class UIManager : MonoBehaviour
 
         if(successDay){
             StageManager.NextStage();
+            ShowInfoUI();
             currentState = "InGame";
         }
         else{
@@ -160,6 +176,57 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    private void ShowInfoUI(){
+        StartCoroutine(ShowInfoCoroutine());
+    }
+    
+    private IEnumerator ShowInfoCoroutine(){
+        targetFrame.gameObject.SetActive(false);
+        targetText.rectTransform.anchoredPosition = new Vector3(0f, -100f, 0f);
+        targetText.rectTransform.localScale = new Vector3(2f, 2f, 2f);
+        targetText.color = Color.white;
+        dayInfoText.rectTransform.anchoredPosition = new Vector3(0f, 100f, 0f);
+        dayInfoText.fontSize = 70;
+        targetText.text = "목표 금액: " + MoneyManager.TargetMoneyList[StageManager.CurrentStage].ToString() + "$";
+        dayInfoText.text = "Day " + StageManager.CurrentStage;
+        blackScreen.gameObject.SetActive(true);
+
+        dayInfoText.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(2f);
+
+        float elapsedTime = 0f;
+        while (elapsedTime < 1f)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Clamp01(1 - elapsedTime / 1f);
+            SetTextAlpha(dayInfoText, alpha);
+            SetTextAlpha(targetText, alpha);
+
+            
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        SetTextAlpha(dayInfoText, 1f);
+        SetTextAlpha(targetText, 1f);
+
+        blackScreen.gameObject.SetActive(false);
+        targetFrame.gameObject.SetActive(true);
+        targetText.rectTransform.anchoredPosition = new Vector3(-300f, -225f, 0f);
+        targetFrame.rectTransform.anchoredPosition = targetText.rectTransform.anchoredPosition;
+        targetText.color = Color.black;
+        targetText.rectTransform.localScale = new Vector3(1f, 1f, 1f);
+        dayInfoText.rectTransform.anchoredPosition = new Vector3(150f, -285f, 0f);
+        dayInfoText.fontSize = 36;
+
+    }
+
+    private void SetTextAlpha(TextMeshProUGUI text, float alpha)
+    {
+        Color color = text.color;
+        color.a = alpha;
+        text.color = color;
+    }
 
     private void SettingsButtonClick(){
         previousState = currentState;
